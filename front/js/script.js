ddd642f022d7da6592bb3124643300c9a0128616
Alexandra Mallet <malletalexandra@gmail.com>
2022-07-28 18:08:02 +0200

    insertion produits page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,14 +1,33 @@
-fetch("http://localhost:3000/api/products")
-    .then(function(res){
-        if (res.ok) {
-            return res.json();
-          }
-        })
-        .then(function(value) {
-          console.log(value);
-        })
-        .catch(function(err) {
-          // Une erreur est survenue
-        });
-    
+async function showItems() {
 
+  const requestURL = 'http://localhost:3000/api/products';
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const products = await response.json();
+
+  getItems(products);
+
+}
+
+function getItems(products) {
+  for (item of products) {
+    let itemsSection = document.getElementById("items");
+    let linkToItem = document.createElement("a");
+    let contenerItem = document.createElement("article");
+    let imgItem = document.createElement("img");
+    let nameItem = document.createElement("h3");
+    let descrItem = document.createElement("p");
+    itemsSection.appendChild(linkToItem).appendChild(contenerItem);
+    contenerItem.appendChild(imgItem);
+    contenerItem.appendChild(nameItem);
+    contenerItem.appendChild(descrItem);
+    imgItem.setAttribute("src", item.imageUrl, "alt", item.altTxt);
+    nameItem.classList.add("productName");
+    descrItem.classList.add("productDescription");
+    nameItem.textContent=item.name;
+    descrItem.textContent=item.description;
+  }
+}
+
+showItems();
